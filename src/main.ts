be2b7491185a7756daac092da7afafd128384922
oxypomme <oxytom8@gmail.com>
2022-05-15 12:29:44 +0200

    feat: :heavy_plus_sign: Added PWA

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import "./registerServiceWorker";
 
 loadFonts();
 

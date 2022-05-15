b7bf2ff05df31ee4436405729a5668e16ae75b68
oxypomme <oxytom8@gmail.com>
2022-05-15 12:21:22 +0200

    feat: :heavy_plus_sign: Added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>Welcome to Your Vue.js + TypeScript App</h1>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+
+  components: {
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
-createApp(App).use(store).mount("#app");
+loadFonts();
+
+createApp(App).use(store).use(vuetify).mount("#app");
